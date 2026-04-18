77c512b500e76dadae08688c7a19b9782764b5d4
ashish-io-glitch <ashishkumar13622631@gmail.com>
2026-04-18 22:17:55 +0530

    Patch Express 5 routing syntax

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/api/orders', async (req, res) => {
 });
 
 // Fallback to index.html
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
